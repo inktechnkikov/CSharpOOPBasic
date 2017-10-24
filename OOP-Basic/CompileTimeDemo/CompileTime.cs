af949609c76b759d9351babd4ae0a27cc89637c6
Nedyalko Kikov <inktech.n.kikov@gmail.com>
2017-10-24 20:35:53 +0300

    Add overloading demo

---
+++ OOP-Basic/CompileTimeDemo/CompileTime.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompileTimeDemo
+{
+    class CompileTime
+    {
+        static void Main(string[] args)
+        {
+            MathOperations operations = new MathOperations();
+            int firstResult = operations.Add(4, 4);
+            int secondResult = operations.Add(4, 5, 5);
+            Console.WriteLine(firstResult);
+            Console.WriteLine(secondResult);
+        }
+    }
+}
---
+++ OOP-Basic/CompileTimeDemo/MathOperations.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompileTimeDemo
+{
+    class MathOperations
+    {
+        //Overloading methods
+        public int Add(int a,int b)
+        {
+            return a + b;
+        }
+        public int Add(int a, int b,int c)
+        {
+            return a + b + c;
+        }
+
+    }
+}
